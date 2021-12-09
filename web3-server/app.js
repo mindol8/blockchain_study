812dfd19fba8d33954f95e85785f86187fd003b8
autoininc <37296580+mindol8@users.noreply.github.com>
2021-12-09 11:11:00 +0900

    new_update

--- web3-server/app.js
+++ web3-server/app.js
@@ -1,35 +1,36 @@
 import express from "express";
 import Web3 from "web3";
+import Contract from 'web3-eth-contract';
 const app = express();
 const port = 8080;
 const URL = "http://127.0.0.1:7545";
 
-const getWeb3 = () =>{
+const getWeb3 = () => {
     const web3 = new Web3(new Web3.providers.HttpProvider(URL));//http에서 동작하는 해당 URL을 가지는 node에 연결
     return web3;
 }
 
-const getAccount = async () =>{
-    try{
+const getAccount = async () => {
+    try {
         const web3 = getWeb3();
-        const accounts = await web3.eth.getAccounts(); 
+        const accounts = await web3.eth.getAccounts();
         return accounts;
-    }catch (e){
+    } catch (e) {
         return e;
     }
 }
 
-const getGasPrice = async () =>{
+const getGasPrice = async () => {
     try {
         const web3 = getWeb3();
         const gasPrice = await web3.eth.getGasPrice();
-        return  gasPrice;
+        return gasPrice;
     } catch (error) {
         return error;
     }
 }
 
-const getBlock = async () =>{
+const getBlock = async () => {
     try {
         const web3 = getWeb3();
         const block = await web3.eth.getBlock("latest");
@@ -43,27 +44,61 @@ const getBlock = async () =>{
         return error;
     }
 }
-app.get('/',(req,res)=>{
+
+const getHelloWorld = async () => {
+    try {
+        const abi = [
+            {
+                "inputs": [],
+                "name": "renderHelloWorld",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "greeting",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "pure",
+                "type": "function"
+            }
+        ];
+
+        const address = "0x94Dc41d9Ac4Ddfa53D6086CD75E4e1646ce5494C";
+        Contract.setProvider('http://127.0.0.1:7545');
+        const contract = new Contract(abi, address);
+        const result = await contract.methods.renderHelloWorld().call();
+        console.log(result);
+        return result;
+    } catch (error) {
+        return error;
+    }
+}
+app.get('/', (req, res) => {
     getAccount()
-    .then(acc=>{
-        res.send(acc);
-    })
+        .then(acc => {
+            res.send(acc);
+        })
 })
 
-app.get('/gasprice',(req,res)=>{
+app.get('/gasprice', (req, res) => {
     getGasPrice()
-    .then(gas=>{
-        res.send(gas);
-    })
+        .then(gas => {
+            res.send(gas);
+        })
 })
 
-app.get('/getblock',(req,res)=>{
+app.get('/getblock', (req, res) => {
     getBlock()
-    .then(block=>{
-        res.send(block);
-    })
+        .then(block => {
+            res.send(block);
+        })
 })
-
-app.listen(port,()=>{
+app.get('/helloworld', (req, res) => {
+    getHelloWorld()
+        .then(text => {
+            res.send(text);
+        })
+})
+app.listen(port, () => {
     console.log(`running server port: ${port}`);
 })
